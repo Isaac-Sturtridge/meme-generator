2addd87bb60c3d692e7660c133e31c24299f4360
Isaac Sturtridge <eyes330@yahoo.co.uk>
2023-04-10 13:23:56 +0800

    init & header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/header"
+import './style.css'
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+}
+
